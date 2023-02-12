77af1d3bbfadf5ca72f1dc56a40c29431da578d8
Viacheslav Belov <vektorification@gmail.com>
2023-02-12 20:36:42 +0100

    LaunchScreen_setting

--- Sources/ConverterScreen.swift
+++ Sources/ConverterScreen.swift
@@ -1,5 +1,11 @@
 import UIKit
 
 class ConverterScreen: UIViewController {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+    
+    
 }
