7f86d04299a2bbeb44021cdce1d07ed085c34517
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-21 10:34:34 +0100

    Formatting

--- Sources/CurrencyScreen.swift
+++ Sources/CurrencyScreen.swift
@@ -1,42 +1,37 @@
-import UIKit
 import SnapKit
+import UIKit
 
 class CurrencyScreen: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private let tableView = UITableView(frame: .init(x: 50, y: 80, width: 290, height: 550), style: .plain)
     private let backButton = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let viewController = UIViewController()
         viewController.modalPresentationStyle = .fullScreen
         present(viewController, animated: true, completion: nil)
-        
+
         backButton.backgroundColor = .white
         backButton.setTitleColor(.black, for: .normal)
         let buttonBack = NSLocalizedString("buttonBack", comment: "")
         backButton.setTitle(buttonBack, for: .normal)
-        
+
         view.addSubview(tableView)
         view.addSubview(backButton)
-        
-        
+
         backButton.snp.makeConstraints { make in
             make.top.equalTo(view).inset(650)
             make.centerX.equalTo(view)
             make.width.equalTo(120)
             make.height.equalTo(50)
-            
-        
+
             backButton.addAction(UIAction { [weak self] _ in
                 self?.dismiss(animated: true)
             }, for: .primaryActionTriggered)
         }
     }
 
-    
-    
-    
     func tableView(_: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableViewCell", for: indexPath) as? MyTableViewCell
         return cell!
@@ -46,12 +41,5 @@ class CurrencyScreen: UIViewController, UITableViewDataSource, UITableViewDelega
         40
     }
 
-    func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
-    }
-        
-    
-    
+    func tableView(_: UITableView, didSelectRowAt _: IndexPath) {}
 }
-        
-        
--- Sources/TableViewCellForCurrencyScreen.swift
+++ Sources/TableViewCellForCurrencyScreen.swift
@@ -1,41 +1,36 @@
-import UIKit
 import SnapKit
+import UIKit
 
 class MyTableViewCell: UITableViewCell {
-    
     private let shortName = UILabel(frame: .zero)
     private let fullName = UILabel(frame: .zero)
-    
-    
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-    
+
         shortName.textAlignment = .center
         fullName.textAlignment = .center
         contentView.addSubview(shortName)
         contentView.addSubview(fullName)
         shortName.numberOfLines = 0
         fullName.numberOfLines = 0
-        
-        
+
         shortName.snp.makeConstraints { make in
             make.leading.equalTo(contentView.snp.leading)
             make.trailing.equalTo(contentView.snp.trailing)
             make.top.equalTo(contentView.snp.top)
             make.bottom.equalTo(fullName.snp.top)
         }
-        
-        
+
         fullName.snp.makeConstraints { make in
             make.leading.equalTo(contentView.leadingAnchor as! ConstraintRelatableTarget)
             make.trailing.equalTo(contentView.trailingAnchor as! ConstraintRelatableTarget)
             make.bottom.equalTo(contentView.bottomAnchor as! ConstraintRelatableTarget)
         }
     }
-        
-    required init?(coder: NSCoder) {
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
 }
