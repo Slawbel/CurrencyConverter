68a0a23c5eb4b0d25334e1186767a81195779d09
Roman Podymov <podymfrombryansk@yandex.ru>
2023-02-21 11:02:09 +0100

    Formatting

--- Sources/ConverterScreen.swift
+++ Sources/ConverterScreen.swift
@@ -1,5 +1,5 @@
-import UIKit
 import SnapKit
+import UIKit
 
 class ConverterScreen: UIViewController {
     private let inputCurButton = UIButton()
@@ -11,11 +11,11 @@ class ConverterScreen: UIViewController {
     private let buttonCalc = UIButton()
     private let outputLabel = UILabel()
     private let buttonDiagram = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-		view.backgroundColor = .init(named: "mainBackgroundColor")
-       
+        view.backgroundColor = .init(named: "mainBackgroundColor")
+
         inputCurButton.backgroundColor = .white
         inputCurButton.setTitleColor(.black, for: .normal)
         let inputCurBut = NSLocalizedString("inputCurBut", comment: "")
@@ -23,26 +23,26 @@ class ConverterScreen: UIViewController {
         inputCurButton.addAction(UIAction { [unowned self] _ in
             let currencyScreen = CurrencyScreen()
             self.present(currencyScreen, animated: true)
-            
+
         }, for: .primaryActionTriggered)
-        
+
         inputCurLabel.textAlignment = .center
         inputCurLabel.backgroundColor = .white
-        
+
         inputTF.placeholder = NSLocalizedString("writeTheAmount", comment: "")
         inputTF.textAlignment = .center
         inputTF.backgroundColor = .white
-        
+
         swapButton.backgroundColor = .white
         swapButton.setTitleColor(.black, for: .normal)
         let swap = NSLocalizedString("swap", comment: "")
         swapButton.setTitle(swap, for: .normal)
-    
+
         buttonCalc.backgroundColor = .white
         buttonCalc.setTitleColor(.black, for: .normal)
         let calc = NSLocalizedString("convert", comment: "")
         buttonCalc.setTitle(calc, for: .normal)
-        
+
         outputCurButton.backgroundColor = .white
         outputCurButton.setTitleColor(.black, for: .normal)
         let outputCurBut = NSLocalizedString("outputCurBut", comment: "")
@@ -51,18 +51,18 @@ class ConverterScreen: UIViewController {
             let currencyScreen = CurrencyScreen()
             self.present(currencyScreen, animated: true)
         }, for: .primaryActionTriggered)
-        
+
         outputCurLabel.textAlignment = .center
         outputCurLabel.backgroundColor = .white
-        
+
         outputLabel.textAlignment = .center
         outputLabel.backgroundColor = .white
-        
+
         buttonDiagram.backgroundColor = .white
         buttonDiagram.setTitleColor(.black, for: .normal)
         let buttonDiagramText = NSLocalizedString("transfer", comment: "")
         buttonDiagram.setTitle(buttonDiagramText, for: .normal)
-        
+
         view.addSubview(inputCurButton)
         view.addSubview(inputCurLabel)
         view.addSubview(inputTF)
@@ -72,59 +72,59 @@ class ConverterScreen: UIViewController {
         view.addSubview(buttonCalc)
         view.addSubview(outputLabel)
         view.addSubview(buttonDiagram)
-        
-        inputCurButton.snp.makeConstraints{ make in
+
+        inputCurButton.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view).inset(70)
             make.top.equalTo(view).inset(80)
             make.height.equalTo(50)
         }
-        
-        inputCurLabel.snp.makeConstraints{ make in
+
+        inputCurLabel.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view).inset(70)
             make.top.equalTo(view).inset(140)
             make.height.equalTo(40)
         }
-        
+
         inputTF.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view).inset(70)
             make.top.equalTo(view).inset(190)
             make.height.equalTo(50)
             make.centerX.equalTo(view)
         }
-        
-        swapButton.snp.makeConstraints{ make in
+
+        swapButton.snp.makeConstraints { make in
             make.top.equalTo(view).inset(260)
             make.centerX.equalTo(view)
             make.width.equalTo(120)
             make.height.equalTo(50)
         }
-        
-        outputCurButton.snp.makeConstraints{ make in
+
+        outputCurButton.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view).inset(70)
             make.top.equalTo(view).inset(340)
             make.height.equalTo(50)
         }
-        
-        outputCurLabel.snp.makeConstraints{ make in
+
+        outputCurLabel.snp.makeConstraints { make in
             make.leading.trailing.equalTo(view).inset(70)
             make.top.equalTo(view).inset(400)
             make.height.equalTo(40)
         }
-        
-        buttonCalc.snp.makeConstraints{ make in
+
+        buttonCalc.snp.makeConstraints { make in
             make.top.equalTo(view).inset(460)
             make.centerX.equalTo(view)
             make.width.equalTo(120)
             make.height.equalTo(50)
         }
-        
-        outputLabel.snp.makeConstraints{ make in
+
+        outputLabel.snp.makeConstraints { make in
             make.top.equalTo(view).inset(530)
             make.leading.trailing.equalTo(view).inset(70)
             make.height.equalTo(40)
         }
-        
-        buttonDiagram.snp.makeConstraints{ make in
+
+        buttonDiagram.snp.makeConstraints { make in
             make.top.equalTo(view).inset(600)
             make.centerX.equalTo(view)
             make.width.equalTo(120)
--- Sources/CurrencyScreen.swift
+++ Sources/CurrencyScreen.swift
@@ -1,37 +1,37 @@
-import UIKit
 import SnapKit
+import UIKit
 
 class CurrencyScreen: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private let tableView = UITableView()
     private let backButton = UIButton()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let viewController = UIViewController()
         viewController.modalPresentationStyle = .fullScreen
         present(viewController, animated: true, completion: nil)
-        
+
         backButton.backgroundColor = .white
         backButton.setTitleColor(.black, for: .normal)
         let buttonBack = NSLocalizedString("buttonBack", comment: "")
         backButton.setTitle(buttonBack, for: .normal)
-        
+
         view.addSubview(tableView)
         view.addSubview(backButton)
-        
+
         backButton.snp.makeConstraints { make in
             make.top.equalTo(view).inset(650)
             make.centerX.equalTo(view)
             make.width.equalTo(120)
             make.height.equalTo(50)
-            
-        backButton.addAction(UIAction { [weak self] _ in
+
+            backButton.addAction(UIAction { [weak self] _ in
                 self?.dismiss(animated: true)
             }, for: .primaryActionTriggered)
         }
-        
-        tableView.snp.makeConstraints{ make in
+
+        tableView.snp.makeConstraints { make in
             make.top.equalTo(view).inset(80)
             make.bottom.equalTo(view).inset(200)
             make.leading.trailing.equalTo(view).inset(50)
@@ -47,8 +47,5 @@ class CurrencyScreen: UIViewController, UITableViewDataSource, UITableViewDelega
         40
     }
 
-    func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
-    }
+    func tableView(_: UITableView, didSelectRowAt _: IndexPath) {}
 }
-        
-        
