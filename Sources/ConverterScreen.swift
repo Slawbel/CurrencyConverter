f42e9fb435c0c33c0fe19c4722c929540d637a46
Viacheslav Belov <vektorification@gmail.com>
2023-06-19 00:01:50 +0200

    no message

--- Sources/ConverterScreen.swift
+++ Sources/ConverterScreen.swift
@@ -4,15 +4,19 @@ import UIKit
 
 
 class ConverterScreen: UIViewController {
-    
     private let scrollViewMain = UIScrollView()
+    private let nameLabel = UILabel()
+    
     private let stackView = UIStackView()
-    private let inputCurButton = UIButton()
     private let inputCurLabel = UILabel()
+    private let inputCurrencyLabel = UILabel()
+    private let inputCurButton = UIButton()
     private let inputTF = UITextField()
     
-    private let outputCurButton = UIButton()
+    private let stackView2 = UIStackView()
     private let outputCurLabel = UILabel()
+    private let outputCurrencyLabel = UILabel()
+    private let outputCurButton = UIButton()
     private let outputLabel = UILabel()
     
     private let datePicker = UIDatePicker()
@@ -31,62 +35,99 @@ class ConverterScreen: UIViewController {
         
         view.backgroundColor = .init(named: "mainBackgroundColor")
         
+        nameLabel.textAlignment = .center
+        nameLabel.backgroundColor = nil
+        nameLabel.textColor = .white
+        nameLabel.text = NSLocalizedString("nameLabelText", comment: "")
+        nameLabel.font = nameLabel.font.withSize(24)
+        
         stackView.axis = .vertical
         let colorForStackView = hexStringToUIColor(hex: "#181B20")
         stackView.backgroundColor = colorForStackView
         stackView.layer.cornerRadius = 20
         
-        inputCurButton.backgroundColor = .darkGray
+        inputCurLabel.textAlignment = .center
+        inputCurLabel.font = inputCurLabel.font.withSize(14)
+        inputCurLabel.textColor = .white
+        inputCurLabel.backgroundColor = nil
+        inputCurLabel.text = NSLocalizedString("inputCurLabelText", comment: "")
+        
+        inputCurrencyLabel.textAlignment = .center
+        inputCurrencyLabel.font = inputCurrencyLabel.font.withSize(14)
+        inputCurrencyLabel.textColor = .white
+        inputCurrencyLabel.backgroundColor = nil
+        
+        
         inputCurButton.layer.cornerRadius = 10
         let colorForInputCurButton = hexStringToUIColor(hex: "#2B333A")
+        inputCurButton.backgroundColor = colorForInputCurButton
         inputCurButton.setTitleColor(.white, for: .normal)
-        let inputCurBut = NSLocalizedString("inputCurBut", comment: "")
-        inputCurButton.setTitle(inputCurBut, for: .normal)
         inputCurButton.addAction(UIAction { [unowned self] _ in
             let currencyScreen = CurrencyScreen()
             currencyScreen.onCurrencySelectedShort1 = { [weak self] shortName in
                 self?.chosenCurShortName1 = shortName
-                self?.inputCurLabel.text = shortName
+                self?.inputCurrencyLabel.text = shortName
             }
             currencyScreen.modalPresentationStyle = .fullScreen
             self.present(currencyScreen, animated: true)
-
         }, for: .primaryActionTriggered)
 
-        inputCurLabel.textAlignment = .center
-        let colorForInputCurLabelText = hexStringToUIColor(hex: "#FFFEFE")
-        inputCurLabel.textColor = colorForInputCurLabelText
-        inputCurLabel.backgroundColor = nil
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.overrideUserInterfaceStyle = .light
+        let colorForIDatePickerText = hexStringToUIColor(hex: "#2B333A")
+        datePicker.backgroundColor = colorForIDatePickerText
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(ConverterScreen.datePickerValueChanged(_:)), for: .valueChanged)
+        datePicker.layer.cornerRadius = 8
         
         inputTF.placeholder = NSLocalizedString("writeTheAmount", comment: "")
         inputTF.textAlignment = .center
-        inputTF.backgroundColor = .white
+        inputTF.backgroundColor = nil
+        inputTF.textColor = .white
         inputTF.addTarget(self, action: #selector(ConverterScreen.onTextFieldTextChanged(textField:)), for: .editingChanged)
+        
+        
+        
+        
+        stackView2.axis = .vertical
+        let colorForStackView2 = hexStringToUIColor(hex: "#181B20")
+        stackView2.backgroundColor = colorForStackView2
+        stackView2.layer.cornerRadius = 20
 
-        outputCurButton.backgroundColor = .darkGray
+        outputCurLabel.textAlignment = .left
+        outputCurLabel.font = outputCurLabel.font.withSize(14)
+        outputCurLabel.textColor = .white
+        outputCurLabel.backgroundColor = nil
+        outputCurLabel.text = NSLocalizedString("outputCurLabelText", comment: "")
+        
+        outputCurrencyLabel.textAlignment = .center
+        outputCurrencyLabel.font = outputCurrencyLabel.font.withSize(14)
+        let colorForOutputCurrencyLabelText = hexStringToUIColor(hex: "#FFEFE")
+        outputCurrencyLabel.textColor = colorForOutputCurrencyLabelText
+        outputCurrencyLabel.backgroundColor = nil
+        
+        outputCurButton.layer.cornerRadius = 10
+        let colorForOutputCurButton = hexStringToUIColor(hex: "#2B333A")
+        outputCurButton.backgroundColor = colorForOutputCurButton
         outputCurButton.setTitleColor(.white, for: .normal)
-        let outputCurBut = NSLocalizedString("outputCurBut", comment: "")
-        outputCurButton.setTitle(outputCurBut, for: .normal)
         outputCurButton.addAction(UIAction { [unowned self] _ in
             let currencyScreen = CurrencyScreen()
             currencyScreen.onCurrencySelectedShort2 = { [weak self] shortName in
                 self?.chosenCurShortName2 = shortName
-                self?.outputCurLabel.text = shortName
+                self?.outputCurrencyLabel.text = shortName
             }
             currencyScreen.modalPresentationStyle = .fullScreen
             self.present(currencyScreen, animated: true)
         }, for: .primaryActionTriggered)
-
-        outputCurLabel.textAlignment = .center
-        outputCurLabel.backgroundColor = .white
-
-        outputLabel.textAlignment = .center
-        outputLabel.backgroundColor = .white
         
-        datePicker.timeZone = NSTimeZone.local
-        datePicker.backgroundColor = UIColor.white
-        datePicker.datePickerMode = .date
-        datePicker.addTarget(self, action: #selector(ConverterScreen.datePickerValueChanged(_:)), for: .valueChanged)
+        outputLabel.backgroundColor = nil
+        outputLabel.textAlignment = .right
+        outputLabel.textColor = .white
+        outputLabel.font = outputCurrencyLabel.font.withSize(18)
+        outputLabel.text = "0"
+
+        
+        
     
         swapButton.backgroundColor = .darkGray
         swapButton.setTitleColor(.white, for: .normal)
@@ -114,23 +155,35 @@ class ConverterScreen: UIViewController {
             diagramPage.modalPresentationStyle = .fullScreen
             self?.present(diagramPage, animated: true)
         }, for: .primaryActionTriggered)
-
+        
         view.addSubview(scrollViewMain)
-        view.addSubview(stackView)
-        stackView.addArrangedSubview(inputCurButton)
-        stackView.addArrangedSubview(inputCurLabel)
-        //view.addSubview(inputTF)
-        //view.addSubview(outputCurButton)
-        //view.addSubview(outputCurLabel)
-        //view.addSubview(outputLabel)
+        scrollViewMain.addSubview(nameLabel)
+        scrollViewMain.addSubview(stackView)
+        stackView.addSubview(inputCurLabel)
+        stackView.addSubview(inputCurrencyLabel)
+        stackView.addSubview(inputCurButton)
+        stackView.addSubview(datePicker)
+        stackView.addSubview(inputTF)
+        
+        view.addSubview(stackView2)
+        stackView2.addSubview(outputCurLabel)
+        stackView2.addSubview(outputCurrencyLabel)
+        stackView2.addSubview(outputCurButton)
+        stackView2.addSubview(outputLabel)
         //view.addSubview(swapButton)
         //view.addSubview(buttonRateHistory)
-        //view.addSubview(datePicker)
         //view.addSubview(buttonDiagramPage)
         
         scrollViewMain.snp.makeConstraints{ make in
             make.width.height.equalTo(view).priority(.low)
             make.edges.equalTo(view)
+        }
+        
+        nameLabel.snp.makeConstraints{ make in
+            make.width.equalTo(222)
+            make.height.equalTo(40)
+            make.leading.equalTo(scrollViewMain.snp.leading).inset(84)
+            make.top.equalTo(scrollViewMain.snp.top).inset(58)
         }
         
         stackView.snp.makeConstraints{ make in
@@ -139,41 +192,80 @@ class ConverterScreen: UIViewController {
             make.top.equalTo(scrollViewMain.snp.top).inset(115)
             make.height.equalTo(97)
         }
+        
+        inputCurLabel.snp.makeConstraints { make in
+            make.leading.equalTo(view).inset(40)
+            make.width.equalTo(124)
+            make.top.equalTo(view).inset(123)
+            make.height.equalTo(40)
+        }
+        
+        inputCurrencyLabel.snp.makeConstraints { make in
+            make.leading.equalTo(view).inset(77)
+            make.top.equalTo(view).inset(172)
+            make.width.equalTo(32)
+            make.height.equalTo(13)
+        }
 
         inputCurButton.snp.makeConstraints { make in
-            make.leading.equalTo(scrollViewMain.snp.leading).inset(40)
-            make.top.equalTo(scrollViewMain.snp.top).inset(165)
+            make.leading.equalTo(view).inset(38)
+            make.top.equalTo(view).inset(165)
             make.width.equalTo(103)
             make.height.equalTo(28)
         }
-
-        inputCurLabel.snp.makeConstraints { make in
-            make.leading.equalTo(scrollViewMain.snp.leading).inset(40)
-            make.width.equalTo(124)
-            make.top.equalTo(scrollViewMain.snp.top).inset(123)
-            make.height.equalTo(40)
+        
+        datePicker.snp.makeConstraints { make in
+            make.width.equalTo(118)
+            make.height.equalTo(25)
+            make.top.equalTo(view).inset(115)
+            make.leading.equalTo(view).inset(258)
         }
-         /*
+        
         inputTF.snp.makeConstraints { make in
-            make.leading.equalTo(view).inset(170)
-            make.top.equalTo(view).inset(100)
-            make.height.equalTo(80)
-            make.trailing.equalTo(view).inset(0)
+            make.leading.equalTo(view).inset(165)
+            make.top.equalTo(view).inset(160)
+            make.height.equalTo(40)
+            make.width.equalTo(186)
         }
-
-        outputCurButton.snp.makeConstraints { make in
-            make.width.equalTo(80)
-            make.top.equalTo(view).inset(185)
-            make.height.equalTo(80)
+        
+        
+        stackView2.snp.makeConstraints{ make in
+            make.leading.equalTo(view).inset(15)
+            make.top.equalTo(view).inset(218)
+            make.width.equalTo(360)
+            make.height.equalTo(97)
         }
-
+        
         outputCurLabel.snp.makeConstraints { make in
-            make.leading.equalTo(view).inset(80)
-            make.width.equalTo(85)
-            make.top.equalTo(view).inset(185)
-            make.height.equalTo(80)
+            make.leading.equalTo(view).inset(40)
+            make.width.equalTo(255)
+            make.top.equalTo(view).inset(238)
+            make.height.equalTo(17)
+        }
+        
+        outputCurrencyLabel.snp.makeConstraints { make in
+            make.leading.equalTo(view).inset(77)
+            make.width.equalTo(30)
+            make.top.equalTo(view).inset(276)
+            make.height.equalTo(13)
+        }
+        
+        outputCurButton.snp.makeConstraints { make in
+            make.width.equalTo(103)
+            make.top.equalTo(view).inset(268)
+            make.height.equalTo(28)
+            make.leading.equalTo(view).inset(38)
+        }
+        
+        outputLabel.snp.makeConstraints { make in
+            make.width.equalTo(75)
+            make.top.equalTo(view).inset(263)
+            make.height.equalTo(40)
+            make.leading.equalTo(view).inset(276)
         }
 
+        /*
+       
         outputLabel.snp.makeConstraints { make in
             make.leading.equalTo(view).inset(170)
             make.top.equalTo(view).inset(185)
@@ -181,11 +273,7 @@ class ConverterScreen: UIViewController {
             make.trailing.equalTo(view).inset(0)
         }
         
-        datePicker.snp.makeConstraints { make in
-            make.width.equalTo(self.view.frame.width)
-            make.height.equalTo(50)
-            make.top.equalTo(view).inset(270)
-        }
+        
 
         swapButton.snp.makeConstraints { make in
             make.width.equalTo(200)
