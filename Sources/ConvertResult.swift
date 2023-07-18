e2d2fa6615a924c5aec2d906712606c4ea072224
Viacheslav Belov <vektorification@gmail.com>
2023-07-19 01:24:38 +0200

    no message

--- Sources/ConvertResult.swift
+++ Sources/ConvertResult.swift
@@ -4,8 +4,6 @@ struct ConvertResult: Codable {
     let date: String?
     let info: Dictionary<String, Double>
     let result: Double?
-    let result2: Double?
-    let result3: Double?
     let success: Bool?
     
 }
