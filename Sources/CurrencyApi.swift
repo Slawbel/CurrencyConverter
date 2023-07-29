da8f5b85c94f62a29880d2c61cff93f5fc88357d
Roman Podymov <podymfrombryansk@yandex.com>
2023-07-29 08:21:52 +0200

    func conversion

--- Sources/CurrencyApi.swift
+++ Sources/CurrencyApi.swift
@@ -16,8 +16,8 @@ class CurrencyApi: CurrencyApiProtocol {
     public var apiChosenCurShortName4: String!
     public var apiChosenDate: String = ""
 
-    public func conversion2(onCompletion: @escaping (ConvertResult) -> Void) {
-        let string = "https://api.apilayer.com/fixer/convert?to=" + (apiChosenCurShortName2 ?? "") + "&from=" + (apiChosenCurShortName1 ?? "") + "&amount=" + (apiInputTF ?? "0") + "&date=" + apiChosenDate
+    private func conversion(to: String!, onCompletion: @escaping (ConvertResult) -> Void) {
+        let string = "https://api.apilayer.com/fixer/convert?to=" + (to ?? "") + "&from=" + (apiChosenCurShortName1 ?? "") + "&amount=" + (apiInputTF ?? "0") + "&date=" + apiChosenDate
         guard let url = URL(string: string) else {
             return
         }
@@ -32,56 +32,22 @@ class CurrencyApi: CurrencyApiProtocol {
             guard let convertResult = ConvertResult(from: data) else {
                 return
             }
-            DispatchQueue.main.async {                
+            DispatchQueue.main.async {
                 onCompletion(convertResult)
             }
         }
         task.resume()
+    }
+        
+    public func conversion2(onCompletion: @escaping (ConvertResult) -> Void) {
+        conversion(to: apiChosenCurShortName2, onCompletion: onCompletion)
     }
     
     public func conversion3(onCompletion: @escaping (ConvertResult) -> Void) {
-        let string = "https://api.apilayer.com/fixer/convert?to=" + (apiChosenCurShortName3 ?? "") + "&from=" + (apiChosenCurShortName1 ?? "") + "&amount=" + (apiInputTF ?? "0") + "&date=" + apiChosenDate
-        guard let url = URL(string: string) else {
-            return
-        }
-        var request = URLRequest(url:url)
-        request.httpMethod = "GET"
-        request.addValue("mUGIIf6VCrvec8zDdJv2EofmA4euGt2z", forHTTPHeaderField: "apikey")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error  in
-            guard let data = data else {
-                return
-            }
-            print(String(data: data, encoding: .utf8)!)
-            guard let convertResult = ConvertResult(from: data) else {
-                return
-            }
-            DispatchQueue.main.async {
-                onCompletion(convertResult)
-            }
-        }
-        task.resume()
+        conversion(to: apiChosenCurShortName3, onCompletion: onCompletion)
     }
     
     public func conversion4(onCompletion: @escaping (ConvertResult) -> Void) {
-        let string = "https://api.apilayer.com/fixer/convert?to=" + (apiChosenCurShortName4 ?? "") + "&from=" + (apiChosenCurShortName1 ?? "") + "&amount=" + (apiInputTF ?? "0") + "&date=" + apiChosenDate
-        guard let url = URL(string: string) else {
-            return
-        }
-        var request = URLRequest(url:url)
-        request.httpMethod = "GET"
-        request.addValue("mUGIIf6VCrvec8zDdJv2EofmA4euGt2z", forHTTPHeaderField: "apikey")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error  in
-            guard let data = data else {
-                return
-            }
-            print(String(data: data, encoding: .utf8)!)
-            guard let convertResult = ConvertResult(from: data) else {
-                return
-            }
-            DispatchQueue.main.async {
-                onCompletion(convertResult)
-            }
-        }
-        task.resume()
+        conversion(to: apiChosenCurShortName4, onCompletion: onCompletion)
     }
 }
