69d34a79b98190e887bdd76d142d98bcff74cd9b
Roman Podymov <podymfrombryansk@yandex.com>
2023-08-25 13:14:58 +0200

    Some different color

--- Sources/CurrencyScreen.swift
+++ Sources/CurrencyScreen.swift
@@ -161,9 +161,8 @@ class CurrencyScreen: UIViewController, UITableViewDataSource, UITableViewDelega
         testGradientButton()
         
         backButton.layer.cornerRadius = 20
-        backButton.titleLabel!.font = UIFont(name: "DMSans-Regular", size: 16)
-        backButton.titleLabel?.textColor = .white
-        backButton.setTitleColor(.black, for: .normal)
+        backButton.titleLabel?.font = UIFont(name: "DMSans-Regular", size: 30)
+        backButton.setTitleColor(.green, for: .normal)
         let buttonBack = NSLocalizedString("buttonBack", comment: "")
         backButton.setTitle(buttonBack, for: .normal)
                 
