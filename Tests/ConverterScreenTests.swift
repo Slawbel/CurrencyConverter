4d35510691f9bedc5f645a37b57f75281c567a2c
Viacheslav Belov <vektorification@gmail.com>
2024-01-22 21:02:10 +0100

    Absent XCTAssert function

---
+++ Tests/ConverterScreenTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import CurrencyConverter
+
+final class ConverterScreenTests: XCTestCase {
+    
+    var sut: ConverterScreen!
+
+    override func setUp() {
+        super.setUp()
+        sut = ConverterScreen()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func flagShouldBeString() {
+        let sut = ConverterScreen()
+        let bool = sut.flag(country: "aaa")
+        
+        XCTAs
+    }
+
+}
