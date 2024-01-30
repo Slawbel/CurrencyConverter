bb19fb4974baf2d7c9f394e8b12d11ec68842d21
Viacheslav Belov <vektorification@gmail.com>
2024-01-30 23:05:39 +0100

    Not showed data on diagram

--- Sources/DiagramResult.swift
+++ Sources/DiagramResult.swift
@@ -10,16 +10,17 @@ class DiagramResult: DemoBaseViewController {
     
     private let diagramStackView = UIStackView()
     
-    var chartView = LineChartView()
+    lazy var chartView = LineChartView()
+
     var sliderX = UISlider()
     var sliderY = UISlider()
     var sliderTextX = UITextField()
     var sliderTextY = UITextField()
     
-    var chosenCurShortNameBase: String?
-    var chosenCurShortName1: String?
-    var chosenCurShortName2: String?
-    var chosenCurShortName3: String?
+    private var chosenCurShortNameBase: String?
+    private var chosenCurShortName1: String?
+    private var chosenCurShortName2: String?
+    private var chosenCurShortName3: String?
     
     private let labelDiagram = UILabel()
     private let startDatePicker = UIDatePicker()
@@ -34,6 +35,8 @@ class DiagramResult: DemoBaseViewController {
         diagramStackView.axis = .vertical
         diagramStackView.backgroundColor = SetColorByCode.hexStringToUIColor(hex: "#181B20")
         diagramStackView.layer.cornerRadius = 30
+        
+        chartView.backgroundColor = .white
         
         self.options = [.toggleValues,
                         .toggleFilled,
@@ -52,7 +55,6 @@ class DiagramResult: DemoBaseViewController {
                         .toggleAutoScaleMinMax,
                         .toggleData]
 
-        chartView.delegate = self
 
 
         labelDiagram.backgroundColor = .clear
@@ -68,8 +70,8 @@ class DiagramResult: DemoBaseViewController {
         startDatePicker.setDate(.now, animated: true)
         startDatePicker.layerCornerRadius = 15
         startDatePicker.setValue(UIColor.white, forKey: "textColor")
-        //startDatePicker.addTarget(self, action: #selector(rangeOfDates), for: .valueChanged)
-        //startDatePicker.addTarget(self, action: #selector(self.curHistory), for: .valueChanged)
+        startDatePicker.addTarget(self, action: #selector(rangeOfDates), for: .valueChanged)
+        startDatePicker.addTarget(self, action: #selector(self.curHistory), for: .valueChanged)
         
         endDatePicker.timeZone = NSTimeZone.local
         endDatePicker.datePickerMode = .date
@@ -78,9 +80,10 @@ class DiagramResult: DemoBaseViewController {
         endDatePicker.setDate(.now, animated: true)
         endDatePicker.layerCornerRadius = 15
         endDatePicker.setValue(UIColor.white, forKey: "textColor")
-        //endDatePicker.addTarget(self, action: #selector(rangeOfDates), for: .valueChanged)
-        //endDatePicker.addTarget(self, action: #selector(self.curHistory), for: .valueChanged)
+        endDatePicker.addTarget(self, action: #selector(rangeOfDates), for: .valueChanged)
+        endDatePicker.addTarget(self, action: #selector(self.curHistory), for: .valueChanged)
         
+
         chartView.chartDescription.enabled = false
         chartView.dragEnabled = true
         chartView.setScaleEnabled(true)
@@ -128,7 +131,8 @@ class DiagramResult: DemoBaseViewController {
         sliderY.value = 100
 
         chartView.animate(xAxisDuration: 2.5)
-
+        
+        chartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: rangeOfDates())
 
         view.addSubview(diagramStackView)
         diagramStackView.addSubview(chartView)
@@ -183,6 +187,18 @@ class DiagramResult: DemoBaseViewController {
         return dateFormatter.string(from: endDatePicker.date)
     }
     
+    // setting lines on diagram according to each currency
+     func setData(coordinates: [ChartDataEntry], coordinates2 : [ChartDataEntry], coordinates3: [ChartDataEntry], chosenCur1: String, chosenCur2: String, chosenCur3: String) {
+         let set1 = LineChartDataSet(entries: coordinates, label: chosenCur1)
+         let set2 = LineChartDataSet(entries: coordinates2, label: chosenCur2)
+         let set3 = LineChartDataSet(entries: coordinates3, label: chosenCur3)
+         set1.colors = [NSUIColor.blue]
+         set2.colors = [NSUIColor.red]
+         set3.colors = [NSUIColor.orange]
+         let data = LineChartData(dataSets: [set1, set2, set3])
+         chartView.data = data
+     }
+    
     @objc func rangeOfDates() -> [String] {
         var arrayOfDates: [String] = []
         let dayDurationInSeconds: TimeInterval = 60*60*24
@@ -192,6 +208,58 @@ class DiagramResult: DemoBaseViewController {
         }
         print(arrayOfDates)
         return arrayOfDates
+    }
+    
+    @objc func curHistory() {
+        guard let chosenCurShortNameBase = chosenCurShortNameBase else {
+            let alertMissedCurBase = UIAlertController(title: "Missing based currency", message: "Please, select based currency", preferredStyle: .alert)
+            let okActionBase = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertMissedCurBase.addAction(okActionBase)
+            present(alertMissedCurBase, animated:  true, completion: nil)
+            return
+        }
+        
+            
+        // making string with all currencies for API request
+        var symbols = ""
+        if let chosenCurShortName1 = chosenCurShortName1 {
+            symbols += chosenCurShortName1
+        } else {
+            let alertMissedCur1 = UIAlertController(title: "Missing currency 1", message: "Please, select currency #1", preferredStyle: .alert)
+            let okAction1 = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertMissedCur1.addAction(okAction1)
+            present(alertMissedCur1, animated:  true, completion: nil)
+            return
+        }
+        if let chosenCurShortName2 = chosenCurShortName2 {
+            if symbols != "" {
+                symbols += ","
+            }
+            symbols += chosenCurShortName2
+        }
+        if let chosenCurShortName3 = chosenCurShortName3 {
+            if symbols != "" {
+                symbols += ","
+            }
+            symbols += chosenCurShortName3
+        }
+        // api request for all rates during some period
+        let stringUrl = "https://api.apilayer.com/fixer/timeseries?start_date=" + (startChosenDates) + "&end_date=" + (endChosenDates) + "&symbols=" + symbols + "&base=" + (chosenCurShortNameBase)
+        
+        guard let url = URL(string: stringUrl) else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("mUGIIf6VCrvec8zDdJv2EofmA4euGt2z", forHTTPHeaderField: "apikey")
+        
+        guard let data = try? URLSession.shared.dataSync(with: request).0 else {
+            return
+        }
+        //print(String(data: data, encoding: .utf8)!)
+        rateData = RateData(from: data)
+        
+        self.setData(coordinates: coordinates(), coordinates2: coordinates2(), coordinates3: coordinates3(), chosenCur1: chosenCurShortName1 ?? "", chosenCur2: chosenCurShortName2 ?? "", chosenCur3: chosenCurShortName3 ?? "")
     }
     
     func changeDateFormat (dateForChange: Date) -> String {
@@ -307,6 +375,58 @@ class DiagramResult: DemoBaseViewController {
             super.handleOption(option, forChartView: chartView)
         }
     }
+    
+    func coordinates() -> [ChartDataEntry] {
+
+            guard let chosenCurShortName1 = chosenCurShortName1 else {
+                return []
+            }
+            var x = -1
+            let diagramData = (rateData?.rates.sorted(by: { dateAndRateLeft, dateAndRateRight in
+                return dateAndRateLeft.key < dateAndRateRight.key
+            }).compactMap { key, value in
+                guard let currency = value[chosenCurShortName1] else {
+                    return nil as ChartDataEntry?
+                }
+                x += 1
+                return ChartDataEntry(x: Double(x), y: currency)
+            })
+            return diagramData ?? []
+        }
+        
+        func coordinates2() -> [ChartDataEntry] {
+            guard let chosenCurShortName2 = chosenCurShortName2 else {
+                return []
+            }
+            var y = -1
+            let diagramData2 = (rateData?.rates.sorted(by: { dateAndRateLeft, dateAndRateRight in
+                return dateAndRateLeft.key < dateAndRateRight.key
+            }).compactMap { key, value in
+                guard let currency2 = value[chosenCurShortName2] else {
+                    return nil as ChartDataEntry?
+                }
+                y += 1
+                return ChartDataEntry(x: Double(y), y: currency2)
+            })
+            return diagramData2 ?? []
+        }
+        
+        func coordinates3() -> [ChartDataEntry] {
+            guard let chosenCurShortName3 = chosenCurShortName3 else {
+                return []
+            }
+            var z = -1
+            let diagramData3 = (rateData?.rates.sorted(by: { dateAndRateLeft, dateAndRateRight in
+                return dateAndRateLeft.key < dateAndRateRight.key
+            }).compactMap { key, value in
+                guard let currency3 = value[chosenCurShortName3] else {
+                    return nil as ChartDataEntry?
+                }
+                z += 1
+                return ChartDataEntry(x: Double(z), y: currency3)
+            })
+            return diagramData3 ?? []
+        }
 }
 
 extension DiagramResult: ConverterScreenDelegate {
