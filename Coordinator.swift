de7be25ab4c10fb9591da67e268840b2bdc84a1e
Viacheslav Belov <vektorification@gmail.com>
2024-03-11 19:52:57 +0100

    deleted unused file Coordinator, correct Coordinator file was remained

--- Coordinator.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class Coordinator {
-    
-    static func openCurrencyScreen (from: UIViewController, to: UIViewController) {
-        to.modalPresentationStyle = .fullScreen
-        from.navigationController?.pushViewController(to, animated: true)
-    }
-}
