5dc22287b95dab297c158d770decfc9fd6c3458b
Viacheslav Belov <vektorification@gmail.com>
2024-05-23 03:52:47 +0200

    Created DatePickers and LazyGrids

--- Sources/RateScreen/RateScreen.swift
+++ Sources/RateScreen/RateScreen.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 struct RateScreen: View {
     
+    private let columns = [GridItem(.flexible())]
+    @State private var selectedDateStart = Date()
+    @State private var selectedDateEnd = Date()
+    
     init() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
@@ -18,12 +22,82 @@ struct RateScreen: View {
             ZStack {
                 Color.black.ignoresSafeArea(.all)
                 
-                List {
-                    Text("Hello")
-                    Text("Bye")
+                ScrollView {
+                    GeometryReader { geometry in
+                        VStack {
+                            
+                            Spacer(minLength: 30)
+                            
+                            HStack{
+                                DatePicker(
+                                    "",
+                                    selection: $selectedDateStart,
+                                    displayedComponents: [.date]
+                                )
+                                .background(Color.white)
+                                .padding()
+                                .cornerRadius(5)
+                                
+                                DatePicker(
+                                    "",
+                                    selection: $selectedDateEnd,
+                                    displayedComponents: [.date]
+                                )
+                                .background(Color.white)
+                                .padding()
+                                .cornerRadius(5)
+                            }
+                            
+                            Spacer(minLength: 40)
+                            
+                            LazyHGrid(rows: columns, spacing: 5) {
+                                ForEach(0..<4) { index in
+                                    Text("Item \(index)")
+                                        .frame(width: geometry.size.width / 4 - 8, height: 40)
+                                        .background(Color.white)
+                                        .cornerRadius(5)
+                                }
+                            }
+                            .padding()
+                            
+                            HStack(spacing: 10) {
+                                LazyVGrid(columns: columns, spacing: 1) {
+                                    ForEach(0..<12) { index in
+                                        Text("Item \(index)")
+                                            .frame(width: geometry.size.width / 4 - 20, height: 40)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
+                                    }
+                                }
+                                LazyVGrid(columns: columns, spacing: 1) {
+                                    ForEach(0..<12) { index in
+                                        Text("Item \(index)")
+                                            .frame(width: geometry.size.width / 4 - 20, height: 40)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
+                                    }
+                                }
+                                LazyVGrid(columns: columns, spacing: 1) {
+                                    ForEach(0..<12) { index in
+                                        Text("Item \(index)")
+                                            .frame(width: geometry.size.width / 4 - 20, height: 40)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
+                                    }
+                                }
+                                LazyVGrid(columns: columns, spacing: 1) {
+                                    ForEach(0..<12) { index in
+                                        Text("Item \(index)")
+                                            .frame(width: geometry.size.width / 4 - 20, height: 40)
+                                            .background(Color.white)
+                                            .cornerRadius(5)
+                                    }
+                                }
+                            }
+                            .padding()
+                        }
+                    }
                 }
-                .listStyle(PlainListStyle())
-                .padding()
             }
             .navigationBarTitle("Rate History", displayMode: .inline)
         }
