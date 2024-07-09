c24c00b370f9c2295d26a7f4624ea2103baaf2cc
Roman Podymov <podymfrombryansk@yandex.com>
2024-07-09 10:45:56 +0200

    Test sources

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -6,30 +6,40 @@
 //
 
 import XCTest
+@testable import CurrencyConverter
 
 final class CurrencyConverterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var sut: DiagramResult!
+    
+    override func setUp() {
+        super.setUp()
+        sut = DiagramResult(nibName: nil, bundle: nil)
+        sut.loadViewIfNeeded()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testChartViewIsNotNil() {
+        XCTAssertNotNil(sut.chartView)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testChartViewIsSubview() {
+        XCTAssertTrue(sut.chartView.isDescendant(of: sut.view))
     }
-
+    
+    func testChartViewConstraints() {
+        sut.view.layoutIfNeeded()
+        let constraints = sut.chartView.constraints
+        XCTAssertEqual(constraints.count, 4, "Shouldhave 4 constraints installed")
+        XCTAssertTrue(constraints.count == 4)
+        
+        XCTAssertTrue(constraints.allSatisfy { $0.firstItem === sut.chartView && $0.secondItem === sut.diagramStackView})
+        XCTAssertNotNil(constraints.first { $0.firstAttribute == .leading && $0.secondAttribute == .leading && $0.constant == 20})
+        XCTAssertNotNil(constraints.first { $0.firstAttribute == .trailing && $0.secondAttribute == .trailing && $0.constant == 20})
+        XCTAssertNotNil(constraints.first { $0.firstAttribute == .bottom && $0.secondAttribute == .bottom && $0.constant == 45})
+        XCTAssertNotNil(constraints.first { $0.firstAttribute == .height && $0.constant == 540})
+    }
 }
